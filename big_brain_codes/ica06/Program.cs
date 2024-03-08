2e169124013f1f030d3b577e6a76c4b1df34dc24
Uyghur Yaghma <105953305+uyaghma@users.noreply.github.com>
2024-03-08 11:55:59 -0700

    ica06 complete

--- big_brain_codes/ica06/Program.cs
+++ big_brain_codes/ica06/Program.cs
@@ -1,6 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace ica06
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            var app = builder.Build();
+            app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true));
 
-app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Welcome to Tim Hortons!");
 
-app.Run();
+            app.MapPost("/form-process", (Order order) =>
+            {
+                Random rand = new Random();
+                int responseTime = rand.Next(5, 31);
+
+                return new
+                {
+                    Name = order.name,
+                    Location = order.location,
+                    Items = order.item,
+                    Quantity = order.quantity,
+                    Payment = order.payment,
+                    ResponseTime = responseTime
+                };
+            });
+
+            app.Run();
+        }
+        record Order (string name, string location, string item, int quantity, string payment);
+    }
+}
---
+++ big_brain_codes/ica06/client/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    
+    AJAX("https://localhost:7099/", "get", {}, "HTML", ProcessHeaderSuccess, ProcessError);
+    
+    $('.place').click(function () {
+        var url = "https://localhost:7099/form-process";
+        
+        var name = $("#name").val();
+        var location = $("#location").val();
+        var item = $("#menu").val();
+        var quantity = $("#quantity").val();
+        var payment = $("#payment").val();
+        
+        var data = {};
+        data.name = name;
+        data.location = location;
+        data.item = item;
+        data.quantity = quantity;
+        data.payment = payment;
+
+        if (data.name != "" && data.location != "" && data.item != "" && data.quantity != "" && data.payment != "") 
+        {
+            AJAX(url, "post", data, "JSON", ProcessForm, ProcessError);
+        }
+    });
+
+    function AJAX(url, method, data, dataType, successMethod, errorMethod) {
+        let ajaxOptions = {};
+        ajaxOptions['url'] = url;
+        ajaxOptions['method'] = method;
+        ajaxOptions['data'] = JSON.stringify(data);  // NEW for C#
+        ajaxOptions['dataType'] = dataType;
+        ajaxOptions['success'] = successMethod;
+        ajaxOptions['error'] = errorMethod;
+        ajaxOptions['contentType'] = "application/json";  // NEW for C#
+
+        console.log(ajaxOptions);
+        $.ajax(ajaxOptions);
+    }
+
+    function ProcessHeaderSuccess(response, status)
+    {
+        $(".header").html(`<h1 class="welcome" style="text-align: center;">${response}</h1>`);
+    }
+
+    function ProcessForm(response, status)
+    {
+        var html = `Thanks for your order, ${response.name} <br><ol>`;
+        html += `<li>Location: ${response.location}</li>`;
+        html += `<li>Item: ${response.items}</li>`;
+        html += `<li>Quantity: ${response.quantity}</li>`;
+        html += `<li>Payment Method: ${response.payment}</li></ol>`;
+        html += `Your order will be ready in ${response.responseTime} minutes.`;
+
+        $(".order-deets").html(html);
+    }
+
+    function ProcessSuccess(returnedData, status) {
+        console.log("Successful AJAX call");
+        console.log(returnedData);
+        console.log(status);
+    }
+
+    function ProcessError(request, error, messasge) {
+        console.log("AJAX call Error");
+        console.log(error);
+        console.log(messasge);
+    }
+});
